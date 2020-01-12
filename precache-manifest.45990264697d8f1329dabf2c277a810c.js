267b19ae7d306d7f549aea56b68dc209189dfa86
FilipeMts <jfgmatos@outlook.com>
2020-01-12 23:49:51 +0000

    Updates

---
+++ precache-manifest.45990264697d8f1329dabf2c277a810c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98dddcdf1e6d84ea81576d9c6df7548",
+    "url": "/lifeAdvisor/index.html"
+  },
+  {
+    "revision": "c2b671091bf52e0c6880",
+    "url": "/lifeAdvisor/static/css/main.93ce207e.chunk.css"
+  },
+  {
+    "revision": "8257ffa2a540f10960f5",
+    "url": "/lifeAdvisor/static/js/2.4ce34c2d.chunk.js"
+  },
+  {
+    "revision": "c2b671091bf52e0c6880",
+    "url": "/lifeAdvisor/static/js/main.619898ed.chunk.js"
+  },
+  {
+    "revision": "24c095adcfd5c0c5fdad",
+    "url": "/lifeAdvisor/static/js/runtime~main.d91b1e5b.js"
+  }
+]);
